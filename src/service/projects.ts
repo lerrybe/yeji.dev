72e1a28aeb763727d84adf9c1abafc1211f5fc97
lerrybe <lerry597@gmail.com>
2023-08-18 03:58:35 +0900

    feat: modify data

--- src/service/projects.ts
+++ src/service/projects.ts
@@ -41,8 +41,8 @@ export async function getProjectData(fileName: string): Promise<ProjectData> {
     throw new Error(`${fileName}에 해당하는 프로젝트를 찾을 수 없습니다.`);
 
   const index = projects.indexOf(project);
-  const next = index > 0 ? projects[index - 1] : null;
-  const prev = index < projects.length - 1 ? projects[index + 1] : null;
+  const prev = index > 0 ? projects[index - 1] : null;
+  const next = index < projects.length - 1 ? projects[index + 1] : null;
   const content = await readFile(filePath, 'utf-8');
 
   return { ...project, content, next, prev };
