4b8c8f90cf8f7a8a2c8be7b9cdcc9ff85c3126a1
lerrybe <lerry597@gmail.com>
2023-08-17 23:57:43 +0900

    todo: client side, server side sync

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <Header />
         <main>{children}</main>
         <Footer />
