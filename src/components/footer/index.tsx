2e451136391c07176b44e48fbb3a02223b2bf0e0
lerrybe <lerry597@gmail.com>
2023-08-17 17:56:34 +0900

    refactor: remove unnecessary client component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,3 @@
-'use client';
 import * as S from './styles';
 
 export default function Footer() {
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Image from 'next/image';
 import * as S from './styles';
 import { BsGithub } from 'react-icons/bs';
